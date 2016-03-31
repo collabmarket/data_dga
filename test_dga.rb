bd230c67ebf3d28cb39375a75452cd9cf5689402
jrovegno <javier.rovegno@gmail.com>
2016-03-30 22:12:39 -0300

    Clean test_dga

--- test_dga.rb
+++ test_dga.rb
@@ -7,18 +7,6 @@ db.inicio()
 print "db list of methods: " + (db.methods - Object.methods).to_s + "\n"
 print "db.a0 list of methods: " + (db.a0.methods - Object.methods).to_s + "\n"
 
-#~ count = 0
-#~ for r in df.each_index()
-    #~ db.inicio()
-    #~ name = df[:VALUE][r]
-    #~ db.marcar_estacion(name)
-    #~ db.marcar_datos(a1: true)
-    #~ db.marcar_periodo(m3: true)
-    
-    #~ # Time to setup download manager
-    #~ db.a0.wait.time        3    if count == 0
-    #~ count += 1
-#~ end
 db.marcar_estacion('Chungara Ajata')
 db.marcar_datos('a',1)
 db.marcar_periodo('w1')
