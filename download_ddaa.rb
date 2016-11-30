93eb434f822c56adc159f17d5064383aa8a02ea4
jrovegno <javier.rovegno@gmail.com>
2016-11-29 23:35:02 -0300

    Added download ddaa

---
+++ download_ddaa.rb
@@ -0,0 +1,37 @@
+require 'daru'
+require 'fileutils'
+require_relative 'dga'
+
+df = Daru::DataFrame.from_csv 'ddaa.csv'
+db = DgaData.new logger: false
+
+tmpdir = Dir.pwd + '/tmp/'
+descargas = Dir.pwd + '/' + db.a0.conf.outdir + '/'
+rawdata = Dir.pwd + '/rawdata/www.dga.cl/'
+
+#Crea directorio tmp si no existe
+FileUtils.mkdir(tmpdir) if not File.exist?(tmpdir)
+#Crea directorio rawdata si no existe
+FileUtils.mkdir(rawdata) if not File.exist?(rawdata)
+# Descarga datos para todas las estaciones
+all = df.index.entries
+
+# Espera 3 seg
+db.a0.conf.timeout = 3
+
+# Descarga archivos
+for i in all
+    db.a0.open.url          df[0][i]
+    db.a0.wait.time         3
+end
+
+# Mueve archivos descargados
+for i in all
+    filename = df[1][i]
+    if File.exist?(descargas + filename)
+        FileUtils.mv(descargas + filename, rawdata + filename)
+    end
+end
+
+# Close Browser
+db.a0.quit
